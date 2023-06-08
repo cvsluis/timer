fb4f0c199d4cf68a31ac5ed618e28b61723b2b3f
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-07 18:04:08 -0700

    Added comments

--- timer1.js
+++ timer1.js
@@ -1,8 +1,12 @@
+// take command line arguments and sort them in numerical order
 const numbers = process.argv.slice(2);
 numbers.sort((a, b) => { return a - b; });
 
+// loop through numbers
 for (const num of numbers) {
+  // filter the elements that are negative, or are not a number
   if (num >= 0 && !isNaN(num)) {
+    // makes beep at number of miliseconds for the current element
     setTimeout(() => process.stdout.write('\x07'), num * 1000);
   }
 }
